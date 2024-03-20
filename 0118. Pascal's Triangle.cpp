b707471065317cca3ebde945978509795dc758b2
Mrinmoy Saha Joy <78999201+MrinmoyJoy@users.noreply.github.com>
2024-03-20 22:38:34 +0600

    Create 0118. Pascal's Triangle.cpp

---
+++ 0118. Pascal's Triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res;
+        vector<int> prev;
+
+        for(int i=0;i<n;i++){
+            vector<int> curr(i+1,1);
+
+            for(int j=1;j<i;j++){
+                curr[j]=prev[j-1]+prev[j];
+            }
+
+            prev=curr;
+            res.push_back(curr);
+
+
+        }
+        return res;
+
+    }
+
+    
+};
